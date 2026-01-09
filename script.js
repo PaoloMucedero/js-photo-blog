65a0a461522e1202159ec836d4b0c1dcf128be48
PaoloMucedero <p.mucedero@gmail.com>
2026-01-09 13:53:43 +0100

    Aggiornamento annotazioni

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+
+// INIZIO SELEZIONANDO ELEMENTI DOM SUI QUALI AGIRE
 const output = document.getElementsByClassName("row");
+
+
+// SALVO ENDPOINT DELLA CHIAMATA
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
+
+// AVENDO SALVATO ENDPOINT IN UNA COSTANTE POSSO UTILIZZARLO NELLA CHIAMATA AXIOS.GET()
 axios.get(endpoint)
     .then(response => {
         // DA ESEGUIRE PER SUCCESSO
         const result = response.data;
-        console.log(result);
-        
+        console.log(result, response);
+        // QUI SOTTO NON METTE NULLA IN PAGINA PERCHE' COSì STO CHIEDENDO DI METTERE IN PAGINA L'ARRAY DI OGGETTI E NON LE PROPRIETà DI OGNI OGGETTO
+        output.innerHTML = result.response;
+        // DEVO ESTRAPOLARE LE PROPRIETà SCORRENDO L'ARRAY CHE RICEVO
+
     })
     .catch(error => {
         // INTERCETTA ERRORE
